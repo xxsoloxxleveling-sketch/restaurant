a88006a9c9a810aa80a9b6dc4f4b21e0663ab085
Azmaio Admin <xxsoloxxleveling@gmail.com>
2025-11-27 14:15:43 -0500

    Fix build: Replace react-lenis with lenis core

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { ReactLenis } from '@studio-freight/react-lenis';
+import React, { useEffect } from 'react';
+import Lenis from 'lenis';
 
 const SmoothScroll = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
-            {children}
-        </ReactLenis>
-    );
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.5,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            orientation: 'vertical',
+            gestureOrientation: 'vertical',
+            smoothWheel: true,
+            wheelMultiplier: 1,
+            touchMultiplier: 2,
+        });
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
+
+    return <>{children}</>;
 };
 
 export default SmoothScroll;
